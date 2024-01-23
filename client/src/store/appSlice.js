27f4e7ebdfe96fc50e172de455275c9ef615f150
Reinawan <admin@reinawan.com>
2024-01-24 00:33:50 +0700

    feat: setup client

---
+++ client/src/store/appSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+export const appSlice = createSlice({
+    name: 'app',
+    initialState: {
+        
+    },
+    reducers: {
+        
+    }
+})
+
+
+export default appSlice.reducer

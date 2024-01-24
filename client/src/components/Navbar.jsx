97b07b8cef656a462e07234166f5442d039984a6
Reinawan <admin@reinawan.com>
2024-01-24 21:10:18 +0700

    feat: fetch data

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,17 +40,17 @@ const Navbar = () => {
           </div>
         </div>
         <div className="navbar-center">
-          <a className="btn btn-ghost text-xl"><b>HeartSync</b></a>
+          <Link to="/" className="btn btn-ghost text-xl"><b>HeartSync</b></Link>
         </div>
         <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
+          <Link to="/match" className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7.6 8.5h8m-8 3.5H12m7.1-7H5c-.2 0-.5 0-.6.3-.2.1-.3.3-.3.6V15c0 .3 0 .5.3.6.1.2.4.3.6.3h4l3 4 3-4h4.1c.2 0 .5 0 .6-.3.2-.1.3-.3.3-.6V6c0-.3 0-.5-.3-.6a.9.9 0 0 0-.6-.3Z" />
               </svg>
               <span className="badge badge-xs badge-primary indicator-item"></span>
             </div>
-          </button>
+          </Link>
           <label className="swap swap-rotate mx-4">
             <input
               type="checkbox"
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,66 @@
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import React, { useEffect, useState } from 'react';
+
 const HomePage = () => {
+
+  const [userData, setUserData] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  async function showUser() {
+    try {
+      let { data } = await axios({
+        method: 'get',
+        url: "http://localhost:3000/users",
+        headers: {
+          Authorization: 'Bearer ' + localStorage.access_token,
+        },
+      });
+      setUserData(data.data);
+    
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error.response.data.message}`,
+      });
+    }
+  }
+
+  const handleAction = async (action, username) => {
+    try {
+      if (action === "skip") {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % userData.length);
+      } else {
+        await axios.post(
+          `http://localhost:3000/users/${action}/${userData[currentIndex].id}`,
+          {},
+          {
+            headers: {
+              Authorization: 'Bearer ' + localStorage.access_token,
+            },
+          }
+        );
+        Swal.fire({
+          icon: 'success',
+          text: `You ${action} ${username}`,
+        });
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % userData.length);
+      }
+    } catch (error) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: `${error.response.data.message}`,
+      });
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % userData.length);
+    }
+  };
+
+  useEffect(() => {
+    showUser();
+  }, []);
+
   return (
     <>
       <div className="mt-10">
@@ -7,16 +69,22 @@ const HomePage = () => {
         </h2>
         <div className="flex justify-center">
           <div className="card card-side bg-base-100 shadow-xl">
-            <figure>
-              <img src="https://upload.wikimedia.org/wikipedia/id/f/f5/Nami_faace.jpg" alt="Card" />
+            <figure className="w-60 h-60 overflow-hidden">
+              <img
+                src={userData[currentIndex]?.UserProfile?.profilePicture}
+                alt={userData[currentIndex]?.username} />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Nami</h2>
-              <p>Navigator</p>
-              <p>I'm navigating the seas with the Straw Hat Pirates</p>
+              <h2 className="card-title">{userData[currentIndex]?.UserProfile?.fullname}</h2>
+              <p>{userData[currentIndex]?.UserProfile?.occupation}</p>
+              <p>{userData[currentIndex]?.UserProfile?.bio}</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Like</button>
-                <button className="btn btn-primary">Skip</button>
+                <button onClick={() => handleAction('unlike', userData[currentIndex]?.username)}
+                  className="btn btn-primary">UnLike</button>
+                <button onClick={() => handleAction('like', userData[currentIndex]?.username)}
+                  className="btn btn-primary">Like</button>
+                <button onClick={() => handleAction('skip')}
+                  className="btn btn-primary">Skip</button>
               </div>
             </div>
           </div>

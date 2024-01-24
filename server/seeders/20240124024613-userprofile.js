2770b48ff4fb72e845df2a60c5e87a8e006a8a54
Baiquni <baiquni.yahya@gmail.com>
2024-01-24 09:59:06 +0700

    fix: setup server

--- server/seeders/20240124024613-userprofile.js
+++ server/seeders/20240124024613-userprofile.js
@@ -1,25 +1,20 @@
 'use strict';
-
+const fs = require('fs');
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    let data = JSON.parse(fs.readFileSync('./data/userprofiles.json', 'utf-8')).map((el, i) => {
+      el.createdAt = el.updatedAt = new Date()
+      return el
+    })
+    await queryInterface.bulkInsert('UserProfiles', data, {})
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('UserProfiles', null, {
+      truncate: true,
+      restartIdentity: true,
+      cascade: true
+    })
   }
 };

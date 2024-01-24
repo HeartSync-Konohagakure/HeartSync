24ba4ffb8420a218f8476626b8e056db909456dd
Baiquni <baiquni.yahya@gmail.com>
2024-01-24 10:56:04 +0700

    feat: server login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 const { User, UserProfile } = require("../models");
+const { comparePassword } = require('../helpers/bcrypt');
+const { signToken } = require('../helpers/jwt');
 
 class UserController {
     static async register(req,res,next) {
@@ -18,6 +20,35 @@ class UserController {
                 username: data.username,
                 email: data.email,
             })
+        } catch (error) {
+            next(error);
+        }
+    }
+    static async login(req,res,next) {
+        try {
+            let { email, password } = req.body
+            if (!password) {
+                throw { name: "Password is required" }
+            }
+            if (!email) {
+                throw { name: "Email is required" }
+            }
+
+            let options = {}
+            options.where = { email }
+
+            let data = await User.findOne(options)
+            if (!data) {
+                throw { name: "Invalid email/password" }
+            }
+
+            let checkPassword = comparePassword(password, data.password)
+            if (!checkPassword) {
+                throw { name: "Invalid email/password" }
+            }
+
+            let access_token = signToken(data)
+            res.status(200).json({ access_token, username: data.username })
         } catch (error) {
             next(error);
         }
--- server/router/index.js
+++ server/router/index.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
 })
 
 router.post("/register", UserController.register);
+router.post("/login", UserController.login);
 
 router.use(errorHandler);
 

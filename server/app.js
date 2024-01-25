0825948604477944e98176e04d4fe5ff067b6017
Baiquni <baiquni.yahya@gmail.com>
2024-01-25 10:47:27 +0700

    fix: deploy client

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express()
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: "https://heart-sync-e87b6.web.app"
     }
 })
 
